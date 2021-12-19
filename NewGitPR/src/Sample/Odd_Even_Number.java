c89d75c65e4b38b7e8d9375a841d7fa0820df74f
Sumit Salave <Sumit Salave@LAPTOP-P25KPLPS>
2021-12-19 09:38:08 +0530

    Made Changes

---
+++ NewGitPR/src/Sample/Odd_Even_Number.java
@@ -0,0 +1,24 @@
+package Sample;
+
+public class Odd_Even_Number {
+
+	public static void main(String[] args) {
+		int num=11;
+
+		
+		System.out.println(num%2);// will give Remainder
+		System.out.println(num/2);// will give Quotient
+		
+		if(num%2==0)
+		{
+			System.out.println("Number is Even");
+		}
+		else
+		{
+		System.out.println("Number is odd");	
+		}
+	
+		 		
+	}
+
+}
